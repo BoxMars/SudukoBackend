10b354c12813ef4be7161c7bd94f42ab4c819925
Box <box@192.168.10.22>
2022-10-22 22:27:56 +0800

    modified:   api/views.py

--- api/views.py
+++ api/views.py
@@ -1,4 +1,56 @@
-from django.shortcuts import render
+from data import models
+from django.http import JsonResponse
+
+
+def generate_response(data, code):
+    code_ref = {
+        0: ['General Error', 404],
+        1: ['Success', 200],
+        10001: ["School name is not listed", 404],
+        10002: ['Grade name is not listed', 404],
+    }
+    res = {
+        'code': code,
+        'message': code_ref[code][0],
+        data: data
+    }
+    return JsonResponse(res, status=code_ref[code][1])
+
 
 def get_school_list(request):
-    pass
+    data = []
+    school_list = models.School.objects.all()
+    for school in school_list:
+        data.append(school.info())
+    return generate_response(data, 1)
+
+
+def get_grade_list(request):
+    school = request.GET.get('school')
+    school = models.School.objects.filter(name=school)
+    if len(school) == 0:
+        return generate_response({}, 10001)
+    school = school.first()
+    grade_list = models.Grade.objects.filter(school=school)
+    data = []
+    for grade in grade_list:
+        data.append(grade.info())
+    return generate_response(data, 1)
+
+
+def get_class_list(request):
+    school = request.GET.get('school')
+    grade = request.GET.get('grade')
+    school = models.School.objects.filter(name=school)
+    if len(school) == 0:
+        return generate_response({}, 10001)
+    school = school.first()
+    grade = models.Grade.objects.filter(name=grade, school=school)
+    if len(grade) == 0:
+        return generate_response({}, 10002)
+    grade = grade.first()
+    stu_class_list = models.Class.objects.filter(grade=grade)
+    data = []
+    for stu_class in stu_class_list:
+        data.append(stu_class.info())
+    return generate_response(data, 1)
--- data/models.py
+++ data/models.py
@@ -1,35 +1,44 @@
+import uuid
+
 from django.db import models
 
 
 class Paper(models.Model):
+    uid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
     content = models.CharField(max_length=1024)
     answer = models.CharField(max_length=1024)
     notes = models.TextField()
 
 
 class School(models.Model):
+    uid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
     name = models.CharField(max_length=1024)
 
     def info(self):
         content = {
+            'id':self.uid,
             'school': self.name
         }
         return content
 
 
 class Grade(models.Model):
+    uid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
     name = models.CharField(max_length=1024)
     school = models.ForeignKey(School)
 
     def info(self):
         content = {
+            'id':self.uid,
             'grade': self.name,
-            'school': self.school.name
+            'school': self.school.name,
+            'school_id':self.school.uid
         }
         return content
 
 
 class Class(models.Model):
+    uid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
     name = models.CharField(max_length=1024)
     grade = models.ForeignKey(Grade)
 
@@ -38,14 +47,18 @@ class Class(models.Model):
 
     def info(self):
         content = {
+            'id':self.uid
             'class': self.name,
             'grade': self.grade.name,
-            'school': self.get_school().name
+            'grade_id':self.grade.uid,
+            'school': self.get_school().name,
+            'school_id':self.get_school().id,
         }
         return content
 
 
 class User(models.Model):
+    uid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
     name = models.CharField(max_length=1024)
     stuClass = models.ForeignKey(Class)
 
@@ -57,12 +70,14 @@ class User(models.Model):
 
 
 class Exam(models.Model):
+    uid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
     paper = models.ForeignKey(Paper)
 
 
 class Enrollment(models.Model):
+    uid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
     exam = models.ForeignKey(Exam)
     user = models.ForeignKey(User)
     result = models.IntegerField()
